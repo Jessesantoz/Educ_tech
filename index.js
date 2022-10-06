9850d391603bf8bc519374d8a91dcb3bd2f6b2c4
Jessé Santoz <108159237+Jessesantoz@users.noreply.github.com>
2022-10-05 21:46:25 -0300

    Lista
    Lista da comunidade e inicio da aplicação dela em ejs.

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express")
 const app = express()
 const local = 8000
 
+const bodyParser = require('body-parser')
 const routes = require('./routes.js')
 routes(app)
 
 app.use(express.static('./app/public'))
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 
 app.listen(local, function(res, req){
---
+++ lista.js
@@ -0,0 +1,25 @@
+const ListaComunidade = [
+
+    {
+        Nome: 'André',
+        Participação: 'organizador'
+    },
+
+    {
+        Nome: 'Anderson',
+        Participação: 'organizador'
+    },
+
+    {
+         Nome: 'Jessé',
+          Participação: 'organizador'
+     },
+
+     {
+        Nome: 'Lucas',
+         Participação: 'organizador'
+    }
+
+]
+
+module.exports = ListaComunidade
