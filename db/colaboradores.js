9a0fff776fb33bb610420dcbfd6d14b4943dd461
Jessé Santoz <108159237+Jessesantoz@users.noreply.github.com>
2022-10-19 21:47:11 -0200

    Commit
    Aperfeiçoamento do "colaboradores'.

--- db/colaboradores.js
+++ db/colaboradores.js
@@ -16,6 +16,51 @@ const colaboradores = [
      },
 
      {
+        nome: 'Douglas',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Sarah',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Clara',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Diego',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Marcos',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Raniel',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Flavio',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Felipe',
+        valor: '30,00 R$'
+    },
+
+    {
+        nome: 'Flavio',
+        valor: '30,00 R$'
+    },
+
+     {
         nome: 'Zezinho',
         valor: '30,00 R$'
     }
