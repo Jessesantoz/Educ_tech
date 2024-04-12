bfd160c60aec4980bac23dd83e8012dab0df2d2e
Jessé Santoz <j-esse@hotmail.com.br>
2024-04-12 09:09:04 -0300

    Aba de Artigos

--- db/artigos.js
+++ db/artigos.js
@@ -1,23 +1,33 @@
 const artigos = [
 
     {
-        titulo: 'Como a sociedade muda',
-        texto: 'A sociedade contemporânea...'
+        titulo: 'Liquidez',
+        autor: 'Jessé',
+        artigo: 'É uma conceito criado por Zigmand Bauman, que foi projetado inicialmente para comunidades específicas de jogos. O aplicativo Discord está disponível para os sistemas operacionais Microsoft Windows, MacOS, Android, iOS, Linux e em navegadores da Web.[1] Em dezembro de 2016, os desenvolvedores anunciaram que Discord tinha mais de 25 milhões de usuários.' 
     },
-
+    
     {
-        titulo: 'Como a sociedade muda',
-        texto: 'A sociedade brasileira...'
+        titulo: 'Sociedade',
+        autor: 'Jessé', 
+        artigo: 'uvaidnniun nfpiasndvpunas na naofdbvn aonb anbo ansonao noasnb voasnsgvbn vasjdn' 
     },
-
+    
     {
-        titulo: 'Como a sociedade muda',
-        texto: 'A sociedade mundial...'
+        titulo: 'Educação',
+        autor: 'Jessé', 
+        artigo: 'uvaidnniun nfpiasndvpunas na naofdbvn aonb anbo ansonao noasnb voasnsgvbn vasjdn' 
     },
-
+    
     {
-        titulo: 'Como a sociedade muda',
-        texto: 'A sociedade antiga...'
+        titulo: 'Aulas',
+        autor: 'Jessé', 
+        artigo: 'uvaidnniun nfpiasndvpunas na naofdbvn aonb anbo ansonao noasnb voasnsgvbn vasjdn' 
+    },
+    
+    {
+        titulo: 'Tecnologia',
+        autor: 'Jessé', 
+        artigo: 'uvaidnniun nfpiasndvpunas na naofdbvn aonb anbo ansonao noasnb voasnsgvbn vasjdn' 
     }
 
 ]
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const lista = require("./db/lista")
 const colaboradores = require("./db/colaboradores")
 const ferramentas = require("./db/ferramentas")
+const artigos = require("./db/artigos")
 
 
 function routes(app) {
@@ -16,21 +17,35 @@ function routes(app) {
     app.get('/autores', function(req, res) {
         res.render('autores', {lista: lista})
     })
-
+    
+    app.get('/tutoriais', function(req, res) {
+        res.render('tutoriais')
+    })
+    
     app.get('/ferramentas', function(req, res) {
         res.render('ferramentas', {ferramentas: ferramentas})
     })
 
-    app.get('/tutoriais', function(req, res) {
-        res.render('tutoriais')
+    app.get('/artigos', function(req, res) {
+        res.render('artigos', {artigos: artigos})
+    })
+
+    app.get('/ferramenta/:indice', function(req, res) {
+        const indice = req.params.indice
+
+        for(let i =0; i<ferramentas.length; i++){
+            if(indice.toLocaleUpperCase() == (ferramentas[i].titulo).toLocaleUpperCase()){
+        res.render('ferramenta', { ferramentas: ferramentas[i] })
+            }
+        }
     })
 
     app.get('/artigo/:indice', function(req, res) {
         const indice = req.params.indice
 
-        for(let i =0; i<ferramentas.length; i++){
-            if(indice.toLocaleUpperCase() == (ferramentas[i].titulo).toLocaleUpperCase()){
-        res.render('artigo', { ferramentas: ferramentas[i] })
+        for(let i =0; i<artigos.length; i++){
+            if(indice.toLocaleUpperCase() == (artigos[i].titulo).toLocaleUpperCase()){
+        res.render('artigo', { artigos: artigos[i] })
             }
         }
     })
