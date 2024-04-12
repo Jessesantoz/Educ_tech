d95aa931a67a98bf5c69bd2c36ffd4293f5af7ad
Jessé Santoz <j-esse@hotmail.com.br>
2024-04-12 10:10:51 -0300

    mudança no texto de artigo e ferramenta

--- db/ferramentas.js
+++ db/ferramentas.js
@@ -2,7 +2,7 @@ const ferramentas = [
 {
     titulo: 'Discord',
     autor: 'Jessé',
-    artigo: 'Discord é um aplicativo de voz sobre IP (VoIP) e comunicação textual. É uma aplicação gratuita que foi projetado inicialmente para comunidades específicas de jogos. O aplicativo Discord está disponível para os sistemas operacionais Microsoft Windows, MacOS, Android, iOS, Linux e em navegadores da Web.[1] Em dezembro de 2016, os desenvolvedores anunciaram que Discord tinha mais de 25 milhões de usuários.' 
+    artigo: 'Discord é um aplicativo de voz sobre IP (VoIP) e comunicação textual. É uma aplicação gratuita que foi projetado inicialmente para comunidades específicas de jogos. O aplicativo Discord está disponível para os sistemas operacionais Microsoft Windows, MacOS, Android, iOS, Linux e em navegadores da Web.[1] Em dezembro de 2016, os desenvolvedores anunciaram que Discord tinha mais de 25 milhões de usuários. Entre os principais riscos de segurança associados ao uso do Discord por esse público, destaca-se o contato com predadores sexuais. O fato de o áudio no Discord não ser salvo dificulta que os pais obtenham provas concretas caso seus filhos estejam sendo alvos de abordagens por tais pessoas, por exemplo.' 
 },
 
 {
